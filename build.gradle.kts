7dee8547d240ba56f5c39fe725c82263e139af2a
Ricki Hirner <hirner@bitfire.at>
2019-02-06 17:19:01 +0100

    Document used okhttp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
 
 object Libs {
+    // okhttp HTTP library
+    // We'll use 3.12 for now, but this branch won't receive feature updates anymore. Security
+    // updates are limited to Dec 2020, so we'll have to update to 3.13 until then. On Android,
+    // using 3.13 will raise the required SDK level to Android 5.
     const val okhttpVersion = "3.12.1"
+
+    // XmlPullParser library
     const val xpp3Version = "1.1.6"
 }
 
@@ -18,9 +24,10 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
 
-    api("com.squareup.okio:okio:2.+")
+    api("com.squareup.okio:okio:2.+")       // use Kotlin-friendly okhttp 2.x
     api("com.squareup.okhttp3:okhttp:${Libs.okhttpVersion}")
-    implementation("org.ogce:xpp3:${Libs.xpp3Version}")       // XmlPullParser
+
+    implementation("org.ogce:xpp3:${Libs.xpp3Version}")
 
     testImplementation("com.squareup.okhttp3:mockwebserver:${Libs.okhttpVersion}")
 }
