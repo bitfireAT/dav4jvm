47dd6a905bb1feb873dba0390d325d4be517865d
Ricki Hirner <hirner@bitfire.at>
2023-05-12 00:04:47 +0200

    Interpret Last-Modified GMT times as GMT and not as local timezone (#13)
    Closes #12

--- src/test/kotlin/at/bitfire/dav4jvm/HttpUtilsTest.kt
+++ src/test/kotlin/at/bitfire/dav4jvm/HttpUtilsTest.kt
@@ -13,13 +13,18 @@ import org.junit.Test
 class HttpUtilsTest {
 
     @Test
-    fun testFilename() {
+    fun fileName() {
         assertEquals("", HttpUtils.fileName("https://example.com".toHttpUrl()))
         assertEquals("", HttpUtils.fileName("https://example.com/".toHttpUrl()))
         assertEquals("file1", HttpUtils.fileName("https://example.com/file1".toHttpUrl()))
         assertEquals("dir1", HttpUtils.fileName("https://example.com/dir1/".toHttpUrl()))
         assertEquals("file2", HttpUtils.fileName("https://example.com/dir1/file2".toHttpUrl()))
         assertEquals("dir2", HttpUtils.fileName("https://example.com/dir1/dir2/".toHttpUrl()))
+    }
+
+    @Test
+    fun parseDate() {
+        assertEquals(1683825995000, HttpUtils.parseDate("Thu, 11 May 2023 17:26:35 GMT")?.time)
     }
 
 }
