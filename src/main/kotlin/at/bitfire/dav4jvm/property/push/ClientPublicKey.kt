258028647639b2235db440113118062193c9382e
Arnau Mora Gras <arnyminerz@proton.me>
2024-12-02 08:50:53 +0100

    Formatting
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- src/main/kotlin/at/bitfire/dav4jvm/property/push/ClientPublicKey.kt
+++ src/main/kotlin/at/bitfire/dav4jvm/property/push/ClientPublicKey.kt
@@ -1,8 +1,6 @@
 package at.bitfire.dav4jvm.property.push
 
 import at.bitfire.dav4jvm.Property
-import at.bitfire.dav4jvm.PropertyFactory
-import org.xmlpull.v1.XmlPullParser
 
 /**
  * Represents a [NS_WEBDAV_PUSH]`:client-public-key` property.
@@ -10,10 +8,14 @@ import org.xmlpull.v1.XmlPullParser
  * Experimental! See https://github.com/bitfireAT/webdav-push/
  */
 class ClientPublicKey: PushPublicKey() {
+
     companion object {
+
         @JvmField
         val NAME = Property.Name(NS_WEBDAV_PUSH, "client-public-key")
+
     }
+
 
     object Factory : PushPublicKey.Factory<ClientPublicKey>(NAME, ::ClientPublicKey)
 }
--- src/main/kotlin/at/bitfire/dav4jvm/property/push/ServerPublicKey.kt
+++ src/main/kotlin/at/bitfire/dav4jvm/property/push/ServerPublicKey.kt
@@ -8,10 +8,14 @@ import at.bitfire.dav4jvm.Property
  * Experimental! See https://github.com/bitfireAT/webdav-push/
  */
 class ServerPublicKey: PushPublicKey() {
+
     companion object {
+
         @JvmField
         val NAME = Property.Name(NS_WEBDAV_PUSH, "server-public-key")
+
     }
+
 
     object Factory : PushPublicKey.Factory<ServerPublicKey>(NAME, ::ServerPublicKey)
 }
