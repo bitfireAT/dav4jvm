ad0ff00371375d48d7c9285b275b73e0560e548d
Arnau Mora <arnyminerz@proton.me>
2025-02-25 12:54:17 +0100

    Added `property-update`

---
+++ src/main/kotlin/at/bitfire/dav4jvm/property/push/PropertyUpdate.kt
@@ -0,0 +1,46 @@
+package at.bitfire.dav4jvm.property.push
+
+import at.bitfire.dav4jvm.Property
+import at.bitfire.dav4jvm.PropertyFactory
+import at.bitfire.dav4jvm.XmlUtils.propertyName
+import org.xmlpull.v1.XmlPullParser
+
+/**
+ * Represents a [NS_WEBDAV_PUSH]`:property-update` property.
+ *
+ * Experimental! See https://github.com/bitfireAT/webdav-push/
+ */
+class PropertyUpdate(
+    val syncLevel: SyncLevel? = null,
+): Property {
+
+    companion object {
+
+        @JvmField
+        val NAME = Property.Name(NS_WEBDAV_PUSH, "property-update")
+
+    }
+
+
+    object Factory: PropertyFactory {
+
+        override fun getName() = NAME
+
+        override fun create(parser: XmlPullParser): PropertyUpdate {
+            var syncLevel: SyncLevel? = null
+            val depth = parser.depth
+            var eventType = parser.eventType
+            while (!(eventType == XmlPullParser.END_TAG && parser.depth == depth)) {
+                if (eventType == XmlPullParser.START_TAG && parser.depth == depth + 1) {
+                    when (parser.propertyName()) {
+                        SyncLevel.NAME -> syncLevel = SyncLevel.Factory.create(parser)
+                    }
+                }
+                eventType = parser.next()
+            }
+            return PropertyUpdate(syncLevel)
+        }
+
+    }
+
+}
