a78e2d24cf721fc3fe457febed7f100f8d1968ef
Arnau Mora <arnyminerz@proton.me>
2025-02-25 12:54:25 +0100

    Added `depth`

---
+++ src/main/kotlin/at/bitfire/dav4jvm/property/push/Depth.kt
@@ -0,0 +1,38 @@
+package at.bitfire.dav4jvm.property.push
+
+import at.bitfire.dav4jvm.Property
+import at.bitfire.dav4jvm.PropertyFactory
+import at.bitfire.dav4jvm.XmlReader
+import org.xmlpull.v1.XmlPullParser
+
+/**
+ * Represents a [NS_WEBDAV_PUSH]`:depth` property.
+ *
+ * Experimental! See https://github.com/bitfireAT/webdav-push/
+ */
+data class Depth(
+    /** May be `0`, `1` or [Int.MAX_VALUE] (infinite). */
+    val depth: Int? = null
+): Property {
+
+    companion object {
+
+        @JvmField
+        val NAME = Property.Name(NS_WEBDAV_PUSH, "depth")
+
+    }
+
+
+    object Factory: PropertyFactory {
+
+        override fun getName() = NAME
+
+        override fun create(parser: XmlPullParser): Depth {
+            val text = XmlReader(parser).readText()
+            val level = if (text == "infinite") Int.MAX_VALUE else text?.toIntOrNull()
+            return Depth(level)
+        }
+
+    }
+
+}
