f6acc5742da8a08fe762a49ef480464ee3f443c5
Arnau Mora <arnyminerz@proton.me>
2025-02-25 12:55:00 +0100

    Added `sync-level`
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

---
+++ src/main/kotlin/at/bitfire/dav4jvm/property/push/SyncLevel.kt
@@ -0,0 +1,38 @@
+package at.bitfire.dav4jvm.property.push
+
+import at.bitfire.dav4jvm.Property
+import at.bitfire.dav4jvm.PropertyFactory
+import at.bitfire.dav4jvm.XmlReader
+import org.xmlpull.v1.XmlPullParser
+
+/**
+ * Represents a [NS_WEBDAV_PUSH]`:sync-level` property.
+ *
+ * Experimental! See https://github.com/bitfireAT/webdav-push/
+ */
+data class SyncLevel(
+    /** May be `0`, `1` or [Int.MAX_VALUE] (infinite). */
+    val level: Int? = null
+): Property {
+
+    companion object {
+
+        @JvmField
+        val NAME = Property.Name(NS_WEBDAV_PUSH, "sync-level")
+
+    }
+
+
+    object Factory: PropertyFactory {
+
+        override fun getName() = NAME
+
+        override fun create(parser: XmlPullParser): SyncLevel {
+            val text = XmlReader(parser).readText()
+            val level = if (text == "infinite") Int.MAX_VALUE else text?.toIntOrNull()
+            return SyncLevel(level)
+        }
+
+    }
+
+}
