132d6633ed54dac2d900d229a9178305f6e68e84
Ricki Hirner <hirner@bitfire.at>
2026-06-20 17:34:59 +0200

    Replace custom HTTP date parsing/formatting with Ktor functions (#177)
    * Replace custom HTTP date parsing/formatting with Ktor functions
    
    * Remove obsolete date parsing tests

--- src/main/kotlin/at/bitfire/dav4jvm/HttpUtils.kt
+++ src/main/kotlin/at/bitfire/dav4jvm/HttpUtils.kt
@@ -10,43 +10,31 @@
 
 package at.bitfire.dav4jvm
 
-import at.bitfire.dav4jvm.HttpUtils.httpDateFormat
 import io.ktor.http.Url
+import io.ktor.http.fromHttpToGmtDate
+import io.ktor.http.toHttpDate
+import io.ktor.util.date.GMTDate
 import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZoneOffset
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
-import java.util.Locale
 import java.util.logging.Logger
 
 object HttpUtils {
-
-    /**
-     * Preferred HTTP date/time format, see RFC 7231 7.1.1.1 IMF-fixdate
-     */
-    private const val httpDateFormatStr = "EEE, dd MMM yyyy HH:mm:ss z"
-    private val httpDateFormat = DateTimeFormatter.ofPattern(httpDateFormatStr, Locale.US)
 
     private val logger
         get() = Logger.getLogger(javaClass.name)
 
     /**
-     * Formats a date for use in HTTP headers using [httpDateFormat].
+     * Formats a date for use in HTTP headers (RFC 7231 IMF-fixdate).
      *
      * @param date date to be formatted
-     *
      * @return date in HTTP-date format
      */
     fun formatDate(date: Instant): String =
-        ZonedDateTime.ofInstant(date, ZoneId.of("GMT")).format(httpDateFormat)
+        GMTDate(date.toEpochMilli()).toHttpDate()
 
     /**
-     * Parses a HTTP-date according to RFC 7231 section 7.1.1.1.
+     * Parses an HTTP-date according to RFC 7231 section 7.1.1.1.
      *
      * @param dateStr date-time formatted in one of the three accepted formats:
      *
@@ -56,33 +44,12 @@ object HttpUtils {
      *
      * @return date-time, or null if date could not be parsed
      */
-    fun parseDate(dateStr: String): Instant? {
-        val zonedFormats = arrayOf(
-            // preferred format
-            httpDateFormat,
-
-            // obsolete RFC 850 format
-            DateTimeFormatter.ofPattern("EEEE, dd-MMM-yy HH:mm:ss zzz", Locale.US),
-        )
-
-        // try the two formats with zone info
-        for (format in zonedFormats)
-            try {
-                return ZonedDateTime.parse(dateStr, format).toInstant()
-            } catch (ignored: DateTimeParseException) {
-            }
-
-        // try ANSI C's asctime() format
-        try {
-            val formatC = DateTimeFormatter.ofPattern("EEE MMM ppd HH:mm:ss yyyy", Locale.US)
-            val local = LocalDateTime.parse(dateStr, formatC)
-            return local.atZone(ZoneOffset.UTC).toInstant()
-        } catch (ignored: DateTimeParseException) {
-        }
-
-        // no success in parsing
+    fun parseDate(dateStr: String): Instant? = try {
+        val ts = dateStr.fromHttpToGmtDate().timestamp
+        Instant.ofEpochMilli(ts)
+    } catch (_: Exception) {
         logger.warning("Couldn't parse HTTP date: $dateStr, ignoring")
-        return null
+        null
     }
 
 
